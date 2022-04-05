33f656e1b0831ff8c04d11d0064250eb3862a1fa
Patrick Hart <pathart612@gmail.com>
2022-04-04 20:05:28 -0400

    commit tree 465b963a154acc28c3ac0a9aada2f4beea441e58

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = (num1 * num2)
+multiply;
+
+
+let random = Math.floor(Math.random() *10) + 1;
+
+
+
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(10, 18, 2, 20);
+max;
